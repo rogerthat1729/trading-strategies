4c38a53e89899be5be414a7abff754ab281bb136
rogerthat1729 <cs1221603@iitd.ac.in>
2024-02-08 21:26:34 +0530

    wrote S1_2, started S1_3

--- Makefile
+++ Makefile
@@ -4,6 +4,10 @@ SYMBOL	?=	"SBIN"
 strategy ?= "BASIC"
 n ?= 4
 x ?= 4
+p ?= 1
+max_hold_days ?= 28
+c1 ?= 2
+c2 ?= 0.2
 start_date ?= "01/01/2019"
 end_date ?= "01/01/2020"
 
@@ -11,7 +15,7 @@ run:
 	pip3 install -r requirements.txt
 	python3 fetch.py $(SYMBOL) $(start_date) $(end_date) $(n)
 	g++ -std=c++20 -o main main.cpp
-	./main $(strategy) $(n) $(x)
+	./main $(strategy) $(n) $(x) $(p) $(max_hold_days) $(c1) $(c2)
 
 clean: run
 	rm -f main
--- main.cpp
+++ main.cpp
@@ -5,40 +5,11 @@
 // #include <string>
 // #include <ifstream>
 using namespace std;
+#define db double
 
-void S1_1(vector<string> dates, vector<double> prices, int n, int x)
+void make_csv(vector<string> &dates, vector<db> &prices, vector<int> &buy_sell, int portfolio, db final_amt, int n)
 {
     int sz = prices.size();
-    vector<int> changes(sz, 0), buy_sell(sz, 0);
-    for (int i = 1; i < sz; i++)
-    {
-        if (prices[i] > prices[i - 1])
-            changes[i] = changes[i - 1] + 1;
-        else if (prices[i] < prices[i - 1])
-            changes[i] = changes[i - 1] - 1;
-        else
-            changes[i] = changes[i - 1];
-    }
-
-    int portfolio = 0;
-    double final_amt = 0;
-
-    // Clarify what last n days really means
-    for (int i = n; i < sz; i++)
-    {
-        if (changes[i] - changes[i - n] == n && portfolio < x)
-        {
-            portfolio++;
-            buy_sell[i] = -1;
-        }
-        else if (changes[i] - changes[i - n] == -n && portfolio > -x)
-        {
-            portfolio--;
-            buy_sell[i] = +1;
-        }
-        final_amt += buy_sell[i] * prices[i];
-    }
-
     ofstream file_1("daily_cashflow.csv"), file_2("order_statistics.csv"), pnl("final_pnl.txt");
 
     file_1 << "Date,Cashflow\n";
@@ -65,16 +36,137 @@ void S1_1(vector<string> dates, vector<double> prices, int n, int x)
     pnl.close();
 }
 
+void S1_1(vector<string> &dates, vector<db> &prices, int n, int x)
+{
+    int sz = prices.size();
+    vector<int> changes(sz, 0), buy_sell(sz, 0);
+    for (int i = 1; i < sz; i++)
+    {
+        if (prices[i] > prices[i - 1])
+            changes[i] = changes[i - 1] + 1;
+        else if (prices[i] < prices[i - 1])
+            changes[i] = changes[i - 1] - 1;
+        else
+            changes[i] = changes[i - 1];
+    }
+
+    int portfolio = 0;
+    db final_amt = 0;
+
+    // Clarify what last n days really means
+    for (int i = n; i < sz; i++)
+    {
+        if (changes[i] - changes[i - n] == n && portfolio < x)
+        {
+            portfolio++;
+            buy_sell[i] = -1;
+        }
+        else if (changes[i] - changes[i - n] == -n && portfolio > -x)
+        {
+            portfolio--;
+            buy_sell[i] = 1;
+        }
+        final_amt += buy_sell[i] * prices[i];
+    }
+    make_csv(dates, prices, buy_sell, portfolio, final_amt, n);
+}
+
+void S1_2(vector<string> dates, vector<db> prices, int n, int x, db p)
+{
+    int sz = dates.size(), portfolio = 0;
+    db final_amt = 0;
+    vector<int> buy_sell(sz, 0);
+    vector<db> DMA(sz, 0);
+    db sum = 0, sum_sq = 0;
+    for(int i = 0 ; i < n ; ++i)
+    {
+        sum += prices[i];
+        sum_sq += prices[i] * prices[i];
+    }
+    sum /= n;
+    sum_sq /= n;
+    DMA[n-1] = sum;
+    for (int i = n; i < sz; i++)
+    {
+        DMA[i] = DMA[i - 1] + (prices[i] - prices[i - n]) / n;
+        sum_sq += (prices[i] * prices[i] - prices[i - n] * prices[i - n])/n;
+        db sigma = sqrt(sum_sq - DMA[i] * DMA[i]);
+        if (prices[i] > DMA[i] + p * sigma && portfolio < x)
+        {
+            portfolio++;
+            buy_sell[i] = -1;
+        }
+        else if (prices[i] < DMA[i] - p * sigma && portfolio > -x)
+        {
+            portfolio--;
+            buy_sell[i] = 1;
+        }
+        final_amt += buy_sell[i] * prices[i];
+    }
+    make_csv(dates, prices, buy_sell, portfolio, final_amt, n);
+}
+
+void S1_3(vector<string> dates, vector<db> prices, int n, int x, db p, int mhd, db c1, db c2)
+{
+    int sz = dates.size(), portfolio = 0, max_hold_days = 0;
+    db final_amt = 0;
+    vector<int> buy_sell(sz, 0);
+    vector<db> DMA(sz, 0);
+    db sum = 0, sum_sq = 0;
+    for(int i = 0 ; i < n ; ++i)
+    {
+        sum += prices[i];
+        sum_sq += prices[i] * prices[i];
+    }
+    sum /= n;
+    sum_sq /= n;
+    DMA[n-1] = sum;
+    for (int i = n; i < sz; i++)
+    {
+        DMA[i] = DMA[i - 1] + (prices[i] - prices[i - n]) / n;
+        sum_sq += (prices[i] * prices[i] - prices[i - n] * prices[i - n])/n;
+        db sigma = sqrt(sum_sq - DMA[i] * DMA[i]);
+        if (prices[i] > DMA[i] + p * sigma && portfolio < x)
+        {
+            portfolio++;
+            buy_sell[i] = -1;
+        }
+        else if (prices[i] < DMA[i] - p * sigma && portfolio > -x)
+        {
+            portfolio--;
+            buy_sell[i] = 1;
+        }
+        if(buy_sell[i] != 0)
+            max_hold_days = 0;
+        else
+            max_hold_days++;
+        if(max_hold_days >= mhd)
+        {
+            // Change the order_statistics file to reflect value of |buy_sell| > 1
+            buy_sell[i] = portfolio;
+            max_hold_days = 0;
+            portfolio = 0;
+        }
+        final_amt += buy_sell[i] * prices[i];
+    }
+    make_csv(dates, prices, buy_sell, portfolio, final_amt, n);
+}
+
 int main(int argc, char *argv[])
 {
     string strategy = argv[1];
     int n = stoi(argv[2]);
     int x = stoi(argv[3]);
+    db p = stod(argv[4]);
+    int mhd = stoi(argv[5]);
+    db c1 = stod(argv[6]);
+    db c2 = stod(argv[7]);
+    
     ifstream file("data.csv");
     string line;
 
     vector<string> dates;
-    vector<double> prices;
+    vector<db> prices;
 
     std::getline(file, line);
 
@@ -83,7 +175,7 @@ int main(int argc, char *argv[])
         istringstream ss(line);
         string field;
         string date;
-        double price;
+        db price;
         // remove index
         getline(ss, field, ',');
         // Get the date
@@ -98,8 +190,10 @@ int main(int argc, char *argv[])
     }
     file.close();
     if (strategy == "BASIC")
-    {
         S1_1(dates, prices, n, x);
-    }
+    else if(strategy == "DMA")
+        S1_2(dates, prices, n, x, p);
+    else if(strategy == "DMA++")
+        S1_3(dates, prices, n, x, p, mhd, c1, c2);
     return 0;
 }
